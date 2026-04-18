4c9fa7726be4fafd8a7ac35d84b3ba1f91efd88e
AViewFromTheTop <AViewFromTheTop@users.noreply.github.com>
2026-04-18 16:54:55 +0000

    Apply automatic changes

--- src/main/java/net/frozenblock/trailiertales/references/TTBlockEntityTypeIds.java
+++ src/main/java/net/frozenblock/trailiertales/references/TTBlockEntityTypeIds.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2025-2026 FrozenBlock
+ * Copyright 2026 FrozenBlock
  * This file is part of Trailier Tales.
  *
  * This program is free software; you can modify it under
- * the terms of version 1 of the FrozenBlockItemId Modding Oasis License
- * as published by FrozenBlockItemId Modding Oasis.
+ * the terms of version 1 of the FrozenBlock Modding Oasis License
+ * as published by FrozenBlock Modding Oasis.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * FrozenBlockItemId Modding Oasis License for more details.
+ * FrozenBlock Modding Oasis License for more details.
  *
- * You should have received a copy of the FrozenBlockItemId Modding Oasis License
+ * You should have received a copy of the FrozenBlock Modding Oasis License
  * along with this program; if not, see <https://github.com/FrozenBlock/Licenses>.
  */
 
